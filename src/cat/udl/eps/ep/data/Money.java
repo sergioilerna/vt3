745627f1bde90fd792b97b4d8e4e4838be5606fa
Alvaro Ortega Marmol <alortegama@outlook.com>
2017-12-23 08:35:41 +0100

    JavaDoc Money.class
    Javadoc de la classe Money finalitzat

--- src/cat/udl/eps/ep/data/Money.java
+++ src/cat/udl/eps/ep/data/Money.java
@@ -3,31 +3,68 @@ package cat.udl.eps.ep.data;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+/**
+ * Representa una quantitat monetaria en una determinada divisa
+ *
+ * @author Alvaro Ortega Marmol - 53399228-J
+ */
 public class Money {
     private BigDecimal quantity;
     private Currency currency;
 
+    /**
+     * Constructor
+     *
+     * @param quantity quantitat monteria en una divisa
+     * @param currency divisa corresponent a la moneda
+     */
     public Money(BigDecimal quantity, Currency currency) {
         this.quantity = quantity;
         this.currency = currency;
     }
 
+    /**
+     * Suma de dos operacions monetaries (de la mateixa divisa).
+     * Les monedes han de tenir la mateixa divisa, en cas de que les divises siguin diferents es produira la excepcio
+     * IllegalArgumentException
+     *
+     * @param other Una moneda amb una quantitat
+     * @return Moneda resultant de la operacio
+     */
     public Money add(Money other) {
         if (!(other.currency.equals(currency)))
             throw new IllegalArgumentException("Suma sobre divisies diferents");
         return new Money(this.quantity.add(other.quantity).setScale(2, RoundingMode.UP), this.currency);
     }
 
+    /**
+     * Resta de dos operacions monetaries (de la mateixa divisa).
+     * Les monedes han de tenir la mateixa divisa, en cas de que les divises siguin diferents es produira la excepcio
+     * IllegalArgumentException
+     * @param other Una moneda amb una quantitat
+     * @return Moneda resultant de la operacio
+     */
     public Money subtract(Money other) {
         if (!(other.currency.equals(currency)))
             throw new IllegalArgumentException("Resta sobre divisies diferents");
         return new Money(this.quantity.subtract(other.quantity).setScale(2, RoundingMode.UP), this.currency);
     }
 
+    /**
+     * Producte d'na quantitat monetaria por un enter
+     * @param multiplier enter multiplicador
+     * @return Moneda resultant de la operacio
+     */
     public Money multiply(int multiplier) {
         return new Money(this.quantity.multiply(new BigDecimal(multiplier)).setScale(2, RoundingMode.UP), this.currency);
     }
 
+    /**
+     * Producte d'una quantitat monetaria per un rati de canvi, expressant el resultat en la divisa donada
+     * @param ratio rati multiplicador de la divisa
+     * @param to divisa a la qual es vol realitzar el canvi
+     * @return Moneda amb la quantitat corresponent al rati i la divisa donada
+     */
     public Money change(BigDecimal ratio, Currency to) {
         if (to.equals(this.currency))
             throw new IllegalArgumentException("No es pot fer el canvi a la mateixa moneda");
