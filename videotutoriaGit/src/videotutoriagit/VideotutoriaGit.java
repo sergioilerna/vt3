a783a6be79f9c3d2cb51d6ef6093c40c72935399
smalo <smalo@PTL044.ilerna.online>
2020-04-29 08:58:59 +0200

    Primer commit de clase

---
+++ videotutoriaGit/src/videotutoriagit/VideotutoriaGit.java
@@ -0,0 +1,14 @@
+
+package videotutoriagit;
+
+
+public class VideotutoriaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
